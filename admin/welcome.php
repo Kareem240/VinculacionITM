21ebaf54d3d37e2e717f8995bb95d251e3d075de
Karim <jobsai98@gmail.com>
2021-01-11 19:39:35 -0600

    Admin 0.2

--- admin/welcome.php
+++ admin/welcome.php
@@ -8,27 +8,23 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
+    <meta charset="UTF-8"> 
+
+<?php include_once 'head.php'; ?>
+
 </head>
 <body>
     <div class="page-header">
-        <h1>Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenid@ a nuestro sitio.</h1>
+        <br><br><br>
+        <h1 style="text-indent: 80px">Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenid@ al administrador de Vinculacion ITM.</h1>
         
     </div>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Cambia tu contraseña</a>
-        <a href="logout.php" class="btn btn-danger">Cierra la sesión</a>
-        
-        
     </p>
 
 
