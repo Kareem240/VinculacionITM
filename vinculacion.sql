e1b2e3bc226d1fe49399c3476335f5f5bd79873f
Karim <jobsai98@gmail.com>
2021-01-11 20:49:02 -0600

    Arreglos en base de datos

--- vinculacion.sql
+++ vinculacion.sql
@@ -16,6 +16,71 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`vinculacion` /*!40100 DEFAULT CHARACTER
 
 USE `vinculacion`;
 
+/*Table structure for table `destacados` */
+
+DROP TABLE IF EXISTS `destacados`;
+
+CREATE TABLE `destacados` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `carrera` varchar(80) NOT NULL,
+  `text` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `destacados` */
+
+/*Table structure for table `estadisticas` */
+
+DROP TABLE IF EXISTS `estadisticas`;
+
+CREATE TABLE `estadisticas` (
+  `id` int(11) NOT NULL,
+  `estad` varchar(50) NOT NULL,
+  `val` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `estadisticas` */
+
+/*Table structure for table `noticias1` */
+
+DROP TABLE IF EXISTS `noticias1`;
+
+CREATE TABLE `noticias1` (
+  `id` int(11) NOT NULL,
+  `dir` varchar(200) NOT NULL,
+  `text` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `noticias1` */
+
+/*Table structure for table `noticias2` */
+
+DROP TABLE IF EXISTS `noticias2`;
+
+CREATE TABLE `noticias2` (
+  `id` int(11) NOT NULL,
+  `dir` varchar(200) DEFAULT NULL,
+  `text` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `noticias2` */
+
+/*Table structure for table `reportes` */
+
+DROP TABLE IF EXISTS `reportes`;
+
+CREATE TABLE `reportes` (
+  `id` varchar(20) NOT NULL,
+  `dir` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `reportes` */
+
 /*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
