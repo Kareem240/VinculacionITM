9bbb5a8c67b4df60f5f0e093d7c5670866762b76
ALFREDOITM <alfredosanchez.as68@gmail.com>
2021-01-13 19:54:17 -0600

    Modificacion en formulario para editar noticias

--- admin/vistas/editar.php
+++ admin/vistas/editar.php
@@ -12,7 +12,7 @@ $row = $noti->fetch_assoc()
 <form action="" method="post">
 	<input type="text" value="<?php echo $row["id"];?>" name="id">
 	<input type="text" value="<?php echo $row["dir"];?>" name="titulo">
-	<input type="text" value="<?php echo $row["text"];?>" name="texto">
+	<textarea rows = "10" cols = "100" name = "texto"><?php echo $row["text"];?></textarea>
 	<input type="submit" name="submit">
 </form>
 
